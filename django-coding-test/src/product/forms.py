cf6510674e89b4132f9953c218ed89b809060c59
Zehan <zehan.kareem@gmail.com>
2024-03-10 23:03:47 +0600

    Update

--- django-coding-test/src/product/forms.py
+++ django-coding-test/src/product/forms.py
@@ -12,3 +12,10 @@ class VariantForm(ModelForm):
             'description': Textarea(attrs={'class': 'form-control'}),
             'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
         }
+
+class ProductFilterForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Product Title', 'class': 'form-control'}), required=False)
+    variant = forms.CharField(label='', widget=forms.Select(choices=[('', '--Select A Variant--')], attrs={'class': 'form-control'}), required=False)
+    price_from = forms.FloatField(label='', widget=forms.TextInput(attrs={'placeholder': 'From', 'class': 'form-control'}), required=False)
+    price_to = forms.FloatField(label='', widget=forms.TextInput(attrs={'placeholder': 'To', 'class': 'form-control'}), required=False)
+    date = forms.DateField(label='', widget=forms.DateInput(attrs={'placeholder': 'Date', 'class': 'form-control'}), required=False)
--- django-coding-test/src/product/views/product.py
+++ django-coding-test/src/product/views/product.py
@@ -1,7 +1,8 @@
 from django.views import generic
-
-from product.models import Variant
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render
+from product.models import Variant, Product
+from forms import ProductFilterForm
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
@@ -11,4 +12,58 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+    paginate_by = 10  # Set the number of items per page
+    form_class = ProductFilterForm
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        form = self.form_class(self.request.GET)
+        
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            variant = form.cleaned_data.get('variant')
+            min_price = form.cleaned_data.get('min_price')
+            max_price = form.cleaned_data.get('max_price')
+            created_at = form.cleaned_data.get('created_at')
+
+            if title:
+                queryset = queryset.filter(title__icontains=title)
+            if variant:
+                queryset = queryset.filter(productvariant__variant_title__icontains=variant)
+            if min_price:
+                queryset = queryset.filter(productvariantprice__price__gte=min_price)
+            if max_price:
+                queryset = queryset.filter(productvariantprice__price__lte=max_price)
+            if created_at:
+                queryset = queryset.filter(created_at__date=created_at)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = self.form_class(self.request.GET)
+        context['form'] = form
+
+        # Customize the response with the range summary
+        paginator = Paginator(context['products'], self.paginate_by)
+        page = self.request.GET.get('page')
+
+        try:
+            paginated_products = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_products = paginator.page(1)
+        except EmptyPage:
+            paginated_products = paginator.page(paginator.num_pages)
+
+        context['products'] = paginated_products
+        context['start_index'] = paginated_products.start_index()
+        context['end_index'] = paginated_products.end_index()
+        context['total_count'] = paginator.count
+
         return context
